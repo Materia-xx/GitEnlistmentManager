0fbe19de16fd8e1fac7b9c28e259baac7df43c38
Materia <170386+Materia-xx@users.noreply.github.com>
2023-02-07 08:10:52 -0800

    Write commandset vsdev2022 to consider 2 different skus

--- GitEnlistmentManager/DTOs/CommandSets/OpenDevVS2022CommandSet.cs
+++ GitEnlistmentManager/DTOs/CommandSets/OpenDevVS2022CommandSet.cs
@@ -1,4 +1,6 @@
 ﻿using GitEnlistmentManager.DTOs.Commands;
+using System.Collections.Generic;
+using System.IO;
 
 namespace GitEnlistmentManager.DTOs.CommandSets
 {
@@ -12,12 +14,25 @@ namespace GitEnlistmentManager.DTOs.CommandSets
             Verb = "dev2022";
             Filename = "gemdev2022.cmdjson";
 
+            // Check a couple different places for the vsDevCmd.bat take the first one found. There is always the option to override the command set too.
+            // Though we still write a non-existing cmd if it doesn't exist. It won't work, but there will be an example in the default command sets folder to work with.
+            var vsSkus = new List<string>() { "Community", "Enterprise" };
+            var vsDevCmd = @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat";
+            foreach (var vsSku in vsSkus)
+            {
+                var potentialVsDevCmd = @$"C:\Program Files\Microsoft Visual Studio\2022\{vsSku}\Common7\Tools\VsDevCmd.bat";
+                if (File.Exists(potentialVsDevCmd))
+                {
+                    vsDevCmd = potentialVsDevCmd;
+                }
+            }
+
             Commands.Add(
                 new RunProgramCommand()
                 {
-                    Program = "wt", // TODO: if wt isn't installed this will error out // TODO: also only works for community. What about other SKUs?
+                    Program = "wt", // TODO: if wt isn't installed this will error out
                                     // Setting the starting directory and then doing CD later into the right directory isn't necessary here. I'm just keeping it around as an example of how to do the escaping for multiple commands
-                    Arguments = @"-w gem nt --title ""{RepoName}"" --startingDirectory ""{ReposFolder}"" ""%comspec%"" /k \""\""C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat\""&&CD /d \""{EnlistmentDirectory}\""\""",
+                    Arguments = $@"-w gem nt --title ""{{RepoName}}"" --startingDirectory ""{{ReposFolder}}"" ""%comspec%"" /k \""\""{vsDevCmd}\""&&CD /d \""{{EnlistmentDirectory}}\""\""",
                     OpenNewWindow = true,
                     // This actually starts with a working folder of the ReposFolder and later on changes directory to the desired directory
                     // This directory ends up being locked by the terminal no matter what directory you CD to, which then would prevent enlistments from
