3fdbcb0fb1bea3473ab80ea72602bef06b088c1f
Materia-xx <170386+Materia-xx@users.noreply.github.com>
2023-02-06 22:48:30 -0800

    CommandSets as .cs and overridekey (#10)

---
+++ GitEnlistmentManager/DTOs/CommandSets/RecreateFromRemoteCommandSet.cs
@@ -0,0 +1,20 @@
+﻿using GitEnlistmentManager.DTOs.Commands;
+
+namespace GitEnlistmentManager.DTOs.CommandSets
+{
+    public class RecreateFromRemoteCommandSet : CommandSet
+    {
+        public RecreateFromRemoteCommandSet()
+        {
+            Placement = CommandSetPlacement.Repo;
+            OverrideKey = "recreate";
+            RightClickText = "Re-create all from remote";
+            Verb = "recreate";
+            Filename = "gemrecreate.cmdjson";
+
+            Commands.Add(
+                new RecreateFromRemoteCommand()
+            );
+        }
+    }
+}
