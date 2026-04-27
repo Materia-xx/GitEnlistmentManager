1a5406c0a588b11c37c87496d6be912d7f79cc57
Materia-xx <170386+Materia-xx@users.noreply.github.com>
2026-04-27 08:48:23 -0700

    User/materia/wip2/010000.w1 (#36)
    Add vs 2026 and code to default right click items

---
+++ GitEnlistmentManager/CommandSets/OpenVSCodeVS2026CommandSet.cs
@@ -0,0 +1,43 @@
+﻿using GitEnlistmentManager.Commands;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GitEnlistmentManager.CommandSets
+{
+    public class OpenVSCodeVS2026CommandSet : CommandSet
+    {
+        public OpenVSCodeVS2026CommandSet()
+        {
+            Placement = CommandSetPlacement.Enlistment;
+            OverrideKey = "vs2026vscode";
+            RightClickText = "VS 2026 CMD -> VS Code";
+            Verb = "vs2026vscode";
+            Filename = "gemvs2026vscode.cmdjson";
+
+            // Check a couple different places for the vsDevCmd.bat take the first one found. There is always the option to override the command set too.
+            // Though we still write a non-existing cmd if it doesn't exist. It won't work, but there will be an example in the default command sets directory to work with.
+            var vsSkus = new List<string>() { "Community", "Enterprise" };
+            var vsDevCmd = @"C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat";
+            foreach (var vsSku in vsSkus)
+            {
+                var potentialVsDevCmd = @$"C:\Program Files\Microsoft Visual Studio\18\{vsSku}\Common7\Tools\VsDevCmd.bat";
+                if (File.Exists(potentialVsDevCmd))
+                {
+                    vsDevCmd = potentialVsDevCmd;
+                }
+            }
+
+            Commands.Add(
+                new RunProgramCommand()
+                {
+                    Program = "wt",
+                    Arguments = $@"-w gem nt --title ""{{RepoName}}"" --startingDirectory ""{{ReposDirectory}}"" ""%comspec%"" /c \""\""{vsDevCmd}\""&&CD /d \""{{EnlistmentDirectory}}\""\""&&code .",
+                    OpenNewWindow = true,
+                    WorkingDirectory = @"{ReposDirectory}"
+                }
+            );
+
+            Documentation = "Opens VSCode under a Visual Studio 2026 Developer command prompt for the selected enlistment.";
+        }
+    }
+}
